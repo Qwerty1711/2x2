68a8cdac771578ecdb05c72374d63ffcc2047db2
Qwerty1711 <103641412+Qwerty1711@users.noreply.github.com>
2023-02-27 22:51:53 +0000

    Update Module1.vb

--- 2x2/Module1.vb
+++ 2x2/Module1.vb
@@ -12,6 +12,14 @@
                        {6, 6}}}
 
     Sub printCube()
+        Console.WriteLine("faces:")
+        Console.WriteLine(" A")
+        Console.WriteLine("BCD")
+        Console.WriteLine(" E")
+        Console.WriteLine(" F")
+        Console.WriteLine()
+        Console.WriteLine("cube:")
+        
         For i = 0 To 1
             Console.Write("   ")
             For j = 0 To 1
@@ -66,22 +74,22 @@
         While True
             printCube()
             func = Console.ReadLine()
-            If func(0) = "1" Then
+            If func(0) = "A" Then
                 switch("510301201101", func(1))
                 switch("511300200100", func(1))
-            ElseIf func(0) = "2" Then
+            ElseIf func(0) = "B" Then
                 switch("000200400500", func(1))
                 switch("010210410510", func(1))
-            ElseIf func(0) = "3" Then
+            ElseIf func(0) = "C" Then
                 switch("010300401111", func(1))
                 switch("011310400101", func(1))
-            ElseIf func(0) = "4" Then
+            ElseIf func(0) = "D" Then
                 switch("011511411211", func(1))
                 switch("001501401201", func(1))
-            ElseIf func(0) = "5" Then
+            ElseIf func(0) = "E" Then
                 switch("210310501110", func(1))
                 switch("211311500111", func(1))
-            ElseIf func(0) = "6" Then
+            ElseIf func(0) = "F" Then
                 switch("410311001100", func(1))
                 switch("411301000110", func(1))
             End If
